d7f6b2913710ab55be2607357f2f1f19bed1bae4
JohannBalderas <al260832@edu.uaa.mx>
2023-11-30 10:47:06 -0600

    Se agrego pagina "acerca de"

---
+++ nosotros.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nosotros</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+    <header>
+        <h1>Sobre Nosotros</h1>
+    </header>
+
+    <!-- agregar iconos -->
+    <section>
+        <h2>Nuestra Misión</h2>
+        <br>
+        <p>Nuestra misión va más allá de vender zapatillas; nos esforzamos por ser el epicentro de la cultura del calzado, donde la autenticidad y la innovación convergen. Creemos en proporcionar a nuestros clientes más que solo productos; buscamos ofrecer experiencias que inspiren, conecten y celebren la diversidad de estilos de vida.</p>
+        <br>
+    
+        <br><br>
+        <h2>Nuestra Visión</h2>
+        <br>
+        <p>Aspiramos a ser más que un simple destino para adquirir calzado de alta calidad; buscamos ser la fuente de inspiración y empoderamiento para los apasionados por la moda urbana y el estilo de vida activo. Nuestra visión es transformar la manera en que las personas experimentan el mundo del calzado, elevando cada par de zapatillas a una expresión única de individualidad y autenticidad.</p>
+        <br>
+
+        <br><br>
+        <h2>Nuestros Objetivos</h2>
+        <br>
+        <p>Buscamos ser la elección predilecta de los amantes de las zapatillas, ofreciendo una selección incomparable de calzado que abarque desde los clásicos atemporales hasta las últimas innovaciones.</p>
+        <br>
+        <p>Nos esforzamos por mantenernos a la vanguardia de las tendencias, anticipando y satisfaciendo las demandas cambiantes de nuestros clientes.  </p>
+        <br>
+        <p>Buscamos ser reconocidos como líderes en la introducción de diseños únicos y colaboraciones exclusivas que resalten nuestra dedicación a la originalidad y la autenticidad en cada par que ofrecemos.</p>
+        <br>
+        <p>Buscamos crear una experiencia de compra excepcional para nuestros clientes, brindando un servicio personalizado y un ambiente acogedor que celebre la cultura del calzado.</p>
+        <br>
+        <p>Nos comprometemos a superar las expectativas, asegurando que cada visita a nuestra tienda sea más que una transacción; queremos que sea una experiencia memorable.</p>
+        <br>
+        <p>Estamos comprometidos con objetivos concretos que promuevan la ética y la responsabilidad social, contribuyendo así a un sector del calzado más consciente y sostenible.</p>
+    </section>
+
+    <!-- tarjetas con nuestras fotos -->
+    <br><br>
+    <div class="conocenos">
+        <h2>Conócenos</h2>
+        <br>
+        <p>Conoce a los creadores de Placo-Shoes</p>
+    </div>
+    <?php
+    include 'carruselSobreNosotros.php';    
+    ?>
+    
+    <br><br><br><br><br><br><br>
+    <?php
+    include 'piePagina.php';    
+    ?>
+
+
+</body>
+</html>
