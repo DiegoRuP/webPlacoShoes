1bdf263f574ae864038ca0acea0ac7613413a42b
DiegoNava10 <151683046+DiegoNava10@users.noreply.github.com>
2023-12-01 13:16:40 -0600

    Catalogo.php / Tienda Final Creo
    Se agregaron todos los productos, y se termino de implementar la funcion de filtro por genero

--- catalogo.php
+++ catalogo.php
@@ -9,56 +9,57 @@
 </head>
 <body>
     <br>
-    <div class="filtro">
-        <label for="filtroGenero">Filtrar por género:</label>
-        <select id="filtroGenero">
-            <option value="todos">Todos</option>
-            <option value="hombres">Hombres</option>
-            <option value="mujeres">Mujeres</option>
-        </select>
-    </div>
+    
     
     <div class="contenedorCatalogo">
+        <div class="filtro">
+            <label for="filtroGenero">Filtrar por género:</label>
+            <select id="filtroGenero">
+                <option value="todos">Todos</option>
+                <option value="hombres">Hombres</option>
+                <option value="mujer">Mujeres</option>
+            </select>
+        </div>
        <br>
        <div class="cartaPrincipal">
             <ul>
                 <li>
-                <div class="shop-card">
-                    <div class="title">
-                        Nike Travis
+                    <div class="shop-card">
+                        <div class="title">
+                            Nike Travis
+                        </div>
+                    <div class="desc">
+                        Hombres
                     </div>
-                <div class="desc">
-                    Hombres
-                </div>
-                <div class="slider">
-                    <figure>
-                        <img src="media/hombre1.png" alt="hombre1" height="200px">
-                    </figure>
-                </div>
+                    <div class="slider">
+                        <figure>
+                            <img src="media/hombre1.png" alt="hombre1" height="200px">
+                        </figure>
+                    </div>
 
-                <div class="cta">
-                    <div class="price">$21000</div>
-                    <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
-                </div>
+                    <div class="cta">
+                        <div class="price">$21000</div>
+                        <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                    </div>
                 </li>
                 <li>
-                <div class="shop-card">
-                <div class="title">
-                    Nike
-                </div>
-                <div class="desc">
-                    Mujer
-                </div>
-                <div class="slider">
-                    <figure>
-                        <img src="media/mujer1.png" alt="mujer1" height="200px">
-                    </figure>
-                </div>
+                    <div class="shop-card">
+                        <div class="title">
+                            Vans
+                        </div>
+                    <div class="desc">
+                        Hombres
+                    </div>
+                    <div class="slider">
+                        <figure>
+                            <img src="media/hombre2.png" alt="hombre2" height="200px">
+                        </figure>
+                    </div>
 
-                <div class="cta">
-                    <div class="price">$3099</div>
-                    <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
-                </div>
+                    <div class="cta">
+                        <div class="price">$1600</div>
+                        <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                    </div>
                 </li>
                 <li>
                 <div class="shop-card">
@@ -86,61 +87,63 @@
         <div class="cartaPrincipal">
             <ul>
                 <li>
+                    <div class="shop-card">
+                        <div class="title">
+                            Nike
+                        </div>
+                        <div class="desc">
+                            Mujer
+                        </div>
+                        <div class="slider">
+                            <figure>
+                                <img src="media/mujer1.png" alt="mujer1" height="200px">
+                            </figure>
+                        </div>
+
+                        <div class="cta">
+                            <div class="price">$3099</div>
+                            <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                        </div>
+                    </div>
+                
+                </li>
+                <li>
                 <div class="shop-card">
                     <div class="title">
-                        Vans
+                        Nike
                     </div>
-                <div class="desc">
-                    Hombres
-                </div>
-                <div class="slider">
-                    <figure>
-                        <img src="media/hombre2.png" alt="hombre2" height="200px">
-                    </figure>
-                </div>
+                    <div class="desc">
+                        Mujer
+                    </div>
+                    <div class="slider">
+                        <figure>
+                            <img src="media/mujer2.png" alt="mujer2" height="200px">
+                        </figure>
+                    </div>
 
-                <div class="cta">
-                    <div class="price">$1600</div>
-                    <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
-                </div>
-                </li>
-                <li>
-                <div class="shop-card">
-                <div class="title">
-                    Nike
-                </div>
-                <div class="desc">
-                    Mujer
-                </div>
-                <div class="slider">
-                    <figure>
-                        <img src="media/mujer2.png" alt="mujer2" height="200px">
-                    </figure>
-                </div>
+                    <div class="cta">
+                        <div class="price">$2800</div>
+                        <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                    </div>
+                    </li>
+                    <li>
+                    <div class="shop-card">
+                    <div class="title">
+                        Converse
+                    </div>
+                    <div class="desc">
+                        Mujer
+                    </div>
+                    <div class="slider">
+                        <figure>
+                            <img src="media/mujer3.png" alt="mujer3" height="200px">
+                        </figure>
+                    </div>
 
-                <div class="cta">
-                    <div class="price">$2800</div>
-                    <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
-                </div>
-                </li>
-                <li>
-                <div class="shop-card">
-                <div class="title">
-                    Converse
-                </div>
-                <div class="desc">
-                    Mujer
-                </div>
-                <div class="slider">
-                    <figure>
-                        <img src="media/mujer3.png" alt="mujer3" height="200px">
-                    </figure>
-                </div>
-
-                <div class="cta">
-                    <div class="price">$2200</div>
-                    <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
-                </div>
+                    <div class="cta">
+                        <div class="price">$2200</div>
+                        <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                    </div>
                 </li>
                     
             </ul>
@@ -209,23 +212,180 @@
             </ul>
         </div>
 
+        <div class="cartaPrincipal">
+            <ul>
+                <li>
+                    <div class="shop-card">
+                        <div class="title">
+                            Nike
+                        </div>
+                    <div class="desc">
+                        Hombres
+                    </div>
+                    <div class="slider">
+                        <figure>
+                            <img src="media/hombre3.png" alt="hombre3" height="200px">
+                        </figure>
+                    </div>
+
+                    <div class="cta">
+                        <div class="price">$8000</div>
+                        <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                    </div>
+                </li>
+                <li>
+                    <div class="shop-card">
+                        <div class="title">
+                            Nike
+                        </div>
+                    <div class="desc">
+                        Hombres
+                    </div>
+                    <div class="slider">
+                        <figure>
+                            <img src="media/hombre4.png" alt="hombre4" height="200px">
+                        </figure>
+                    </div>
+
+                    <div class="cta">
+                        <div class="price">$10000</div>
+                        <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                    </div>
+                </li>
+                <li>
+                <div class="shop-card">
+                <div class="title">
+                    Jordan
+                </div>
+                <div class="desc">
+                    Hombres
+                </div>
+                <div class="slider">
+                    <figure>
+                        <img src="media/hombre5.png" alt="hombre5" height="200px">
+                    </figure>
+                </div>
+
+                <div class="cta">
+                    <div class="price">$6699</div>
+                    <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                </div>
+                </li>
+                
+            </ul>
+        </div>
+
+        <div class="cartaPrincipal">
+            <ul>
+                <li>
+                <div class="shop-card">
+                    <div class="title">
+                        Louis Vuitton
+                    </div>
+                <div class="desc">
+                    Mujer
+                </div>
+                <div class="slider">
+                    <figure>
+                        <img src="media/mujer7.png" alt="mujer7" height="200px">
+                    </figure>
+                </div>
+
+                <div class="cta">
+                    <div class="price">$12000</div>
+                    <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                </div>
+                </li>
+                <li>
+                <div class="shop-card">
+                <div class="title">
+                    Puma
+                </div>
+                <div class="desc">
+                    Mujer
+                </div>
+                <div class="slider">
+                    <figure>
+                        <img src="media/mujer8.png" alt="mujer8" height="200px">
+                    </figure>
+                </div>
+
+                <div class="cta">
+                    <div class="price">$2350</div>
+                    <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                </div>
+                </li>
+                
+                    
+            </ul>
+        </div>
+
+        <div class="cartaPrincipal">
+            <ul>
+                <li>
+                    <div class="shop-card">
+                        <div class="title">
+                            Adidas
+                        </div>
+                    <div class="desc">
+                        Hombres
+                    </div>
+                    <div class="slider">
+                        <figure>
+                            <img src="media/hombre6.png" alt="hombre6" height="200px">
+                        </figure>
+                    </div>
+
+                    <div class="cta">
+                        <div class="price">$2500</div>
+                        <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                    </div>
+                </li>
+                <li>
+                    <div class="shop-card">
+                        <div class="title">
+                            Nike
+                        </div>
+                    <div class="desc">
+                        Hombres
+                    </div>
+                    <div class="slider">
+                        <figure>
+                            <img src="media/hombre8.png" alt="hombre8" height="200px">
+                        </figure>
+                    </div>
+
+                    <div class="cta">
+                        <div class="price">$11000</div>
+                        <button class="btnCard">Añadir al carrito<span class="bg"></span></button>
+                    </div>
+                </li>
+                
+            </ul>
+        </div>
+
         <!-- Termina contenedor catalogo -->
     </div>
 
     <script>
         document.getElementById('filtroGenero').addEventListener('change', function() {
+            
             var filtro = this.value;
             var cartas = document.querySelectorAll('.cartaPrincipal');
 
             cartas.forEach(function(carta) {
+                carta.style.paddingLeft = '72';
+                carta.style.display = 'block';
                 var generoCarta = carta.querySelector('.desc').innerText.toLowerCase();
                 
                 if (filtro === 'todos' || generoCarta === filtro) {
+                    carta.style.paddingLeft = '72';
                     carta.style.display = 'block';
                 } else {
                     carta.style.display = 'none';
                 }
             });
+            
         });
     </script>
     
