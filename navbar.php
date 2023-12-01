b3123a524594832eaa231456f79218e844347e35
DiegoRuP <96434128+DiegoRuP@users.noreply.github.com>
2023-12-01 14:29:10 -0600

    Creacion del carrito de compras
    Se creó el archivo de carrito.js y se ajustó un poquito el diseño de las cartas de los tenis para darles una animación

--- navbar.php
+++ navbar.php
@@ -16,42 +16,69 @@
             <a href=""><img src="media/logoAzul.png" alt="PlacoLogo"></a>
         </div>
 
-              
         <ul class="listaNav">            
             <li>
-                <span> Tenis chidos, actitud chida, para gente chida </span>
-            </li>        
-            
-            <?php
+                <span> Tenis chidos, actitud chida, para gente chida como Diego</span>
+            </li>    
 
-            if (isset($_SESSION['usuario'])) {
-                //SI LA SESION DEL USUARIO ESTÁ ACTIVA MUESTRA EL NOMBRE Y HABILITA EL CARRITO 
-                //(Se puede utilizar también para habilitar las opciones del admin de control de base de datos)
-                echo '
-                
-                <span>
-                        Bienvenido, ' . $_SESSION['usuario'] . '
-                </span>
-                
-                <li class="micuentaNav">
+                <?php
+                if (isset($_SESSION['usuario'])) {
+                    //SI LA SESION DEL USUARIO ESTÁ ACTIVA MUESTRA EL NOMBRE Y HABILITA EL CARRITO 
+                    //(Se puede utilizar también para habilitar las opciones del admin de control de base de datos)
+                    echo '
+                    
+                    <span>
+                            Bienvenido, ' . $_SESSION['usuario'] . '
+                    </span>
+                    
+                    <li class="micuentaNav">
                     <a href=""> <i class="fa-solid fa-right-from-bracket"></i> Cerrar sesión </a>
-                </li>                   
-                <li>
-                    <a href=""> <i class="fa-solid fa-cart-shopping"></i> Carrito </a>
-                </li>   ';
+                    </li>    
+                    
+                    <!-- CARRITO SOLO CON INICIO DE SESIÓN -->
+                    <li>
+                        <div class="iconoCarrito">
+                            <a><i class="fa-solid fa-cart-shopping"></i> Carrito </a>
+                        </div>
+                        <div class="contenedorCarrito">
+                            <div class="contadorProductos">
+                                <span id="contador"> 0 </span>
+                            </div>
+                            <div class="productosCarrito ocultarCarrito">
+                                <div class="itemCarrito">
+                                    <div class="infoCarrito">
+                                        <span class="cantItem">
+                                            1
+                                        </span>
+                                        <p class="tituloItem">
+                                            Tenis Nike
+                                        </p>
+                                        <p class="precioItem">
+                                            $2220
+                                        </p>
+                                    </div>
+                                    <i class="fa-solid fa-xmark fa-lg" id="iconoCerrar"></i>                    </div>
+                                <div class="totalCarrito">
+                                    <h3>Total:</h3>
+                                    <span class="totalPago">$2220</span>
+                                </div>
+                            </div>
+                        </div>   
+                    </li>';
 
-            } else {
-                echo '
-                <li class="micuentaNav">
-                    <a href="login.php"> <i class="fa-solid fa-user"></i> Mi cuenta </a>
-                </li>                   
-                <li>
-                    <!--Carrito desactivado si no tiene sesión-->
-                    <a href="" class="desactivado"> <i class="fa-solid fa-cart-shopping"></i> Carrito </a>
-                </li>   ';
-            }
-            ?>
-                  
+                } else {
+                    echo '
+                    <li class="micuentaNav">
+                        <a href="login.php"> <i class="fa-solid fa-user"></i> Mi cuenta </a>
+                    </li>                   
+                    <li>
+                        <!--Carrito desactivado si no tiene sesión-->
+                        <a class="desactivado"> <i class="fa-solid fa-cart-shopping"></i> Carrito </a>
+                        
+                    </li>   ';
+                }
+            ?>    
+            
          </ul> 
     
     </div>
@@ -79,6 +106,7 @@
                 </ul>
     
     </header>
+    <script src="js/carrito.js"></script>
 
 </div>
 
