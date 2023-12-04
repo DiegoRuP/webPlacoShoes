0b939a848b8afd21bf9ba3d8ce9074f5a876d85c
DiegoNava10 <151683046+DiegoNava10@users.noreply.github.com>
2023-12-03 18:36:10 -0600

    Funcion subir imagenes desde windows
    Al editar un producto de la tienda podemos elegir una imagen desde nuestro explorador de archivos de windows, etc.

--- cambios.php
+++ cambios.php
@@ -64,13 +64,65 @@
         }
     }
     if(isset($_POST['mod'])){
+        $target_dir = "media/";
+        $target_file = $target_dir . basename($_FILES["imagen2"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        // Verificar si el archivo es una imagen real o un archivo falso
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["imagen2"]["tmp_name"]);
+            if ($check !== false) {
+                echo "El archivo es una imagen - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "El archivo no es una imagen.";
+                $uploadOk = 0;
+            }
+        }
+
+        // Verificar si el archivo ya existe
+        if (file_exists($target_file)) {
+            
+            $nombreimag= htmlspecialchars(basename($_FILES["imagen2"]["name"]));
+            $siete= $nombreimag;
+            $uploadOk = 0;
+        }
+
+        // Verificar el tamaño del archivo
+        if ($_FILES["imagen2"]["size"] > 500000) {
+            echo "Lo siento, tu archivo es demasiado grande.";
+            $uploadOk = 0;
+        }
+
+        // Permitir ciertos formatos de archivo
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif") {
+            echo "Lo siento, solo se permiten archivos JPG, JPEG, PNG y GIF.";
+            $uploadOk = 0;
+        }
+
+        // Verificar si $uploadOk está configurado en 0 por un error
+        if ($uploadOk == 0) {
+            echo "Lo siento, tu archivo no fue subido.";
+        } else {
+            // Si todo está bien, intentar subir el archivo
+            if (move_uploaded_file($_FILES["imagen2"]["tmp_name"], $target_file)) {
+                echo "El archivo " . htmlspecialchars(basename($_FILES["imagen2"]["name"])) . " ha sido subido.";
+                $nombreimag= htmlspecialchars(basename($_FILES["imagen2"]["name"]));
+                $siete= $nombreimag;
+            } else {
+                echo "Lo siento, hubo un error al subir tu archivo.";
+            }
+        }
+    
         $uno=$_POST['id2'];
         $dos=$_POST['nom2'];
         $tres=$_POST['desc2'];
         $cuatro=$_POST['stock2'];
         $cinco=$_POST['precio2'];
         $seis=$_POST['descuen2'];
-        $siete=$_POST['imagen2'];
+        //$siete=$_POST['imagen2'];
         $ocho=$_POST['categ2'];
         $modificar1=$_SESSION['modificar2'];
         $ne="UPDATE productos SET ID='$uno', Nombre='$dos', Descripcion='$tres', Stock='$cuatro', Precio='$cinco', Descuento='$seis', Imagen='$siete', Categoria='$ocho' WHERE id='$modificar1'";
@@ -85,6 +137,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="css/formproducto.css">
     <title>Modificar - PlacoShoes</title>
 </head>
@@ -135,7 +188,7 @@
             </div>
         </div>
         <div class="derecha">
-            <form class="estiloformulario" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='post'>
+            <form class="estiloformulario" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='post' enctype="multipart/form-data">
             <ul class="wrapper">
                 <li>
                 <label class="labcol form-control-label" for="id">ID</label>
@@ -162,8 +215,16 @@
                 <input class="form-control" type="text" id="descuento" name="descuen2" value="<?php echo $_SESSION["descuen"]; ?>">
                 </li>
                 <li class="form-row">
-                <label class="labcol form-control-label" for="imagen">IMAGEN</label>
-                <input class="form-control" type="text" id="imagen" name="imagen2" value="<?php echo $_SESSION["imagen"]; ?>">
+                
+                <!-- <label class="labcol form-control-label" for="imagenu">IMAGEN</label> -->
+                <div class="form-group">
+                    <label for="imagen" class="btn2 btn2-outline-primary">Subir tu foto</label> 
+                    <input type="file" name="imagen2" id="imagen" class="form-control-file"  >
+                </div> 
+                <!-- <label for="imagen2" class="btn2 btn2-outline-primary">Subir tu foto</label>
+                <input type="file" name="imagen2" id="imagen"> -->
+                
+                <!-- <input class="form-control" type="text" id="imagen" name="imagen2" value="<?php echo $_SESSION["imagen"]; ?>?"> -->
                 <?php
                 $img= "media/" . $_SESSION["imagen"];
                 ?>
@@ -176,7 +237,7 @@
                 <input class="form-control" type="text" id="categoria" name="categ2" value="<?php echo $_SESSION["categ"]; ?>">
                 </li>
 
-                <button class="btn2 btn2-outline-primary" type="submit" name="mod">Comfirmar edicion</button>
+                <button class="btn2 btn2-outline-primary" type="submit" name="mod">Confirmar edicion</button>
                 <input type="hidden" name="id_baja" value="<?php echo $_SESSION['id']; ?>">
                 <button class="btn2 btn2-outline-primary" type="submit" value="submit" name="baja">Baja</button>
                 <button class="btn2 btn2-outline-primary" type="button" name="mod" onclick="window.location.href='formproducto.php';">Regresar</button>
