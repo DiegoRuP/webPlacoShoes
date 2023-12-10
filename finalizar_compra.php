2e0b2d61c4271f25a4e45c24a9039dc493ab624e
DiegoNava10 <151683046+DiegoNava10@users.noreply.github.com>
2023-12-09 19:17:10 -0600

    Agregue la pagina para finalizar la compra
    agregue finalizar_compra.php y su hoja de estilo finalizar_compra.css

---
+++ finalizar_compra.php
@@ -0,0 +1,151 @@
+<?php include 'navbar.php'?>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Recuperar los datos del formulario
+        $cantidad = $_POST["quantity"];
+        $total = $_POST["total"];
+
+        // Verificar si el nombre se envió y no es nulo
+        $nombres = isset($_POST["nombre"]) ? $_POST["nombre"] : '';
+
+        // Decodificar la cadena JSON de nombres (si se envió como JSON)
+        $nombresArray = json_decode($nombres);
+
+
+        echo "Cantidad: " . $cantidad . "<br>";
+        echo "Total: " . $total . "<br>";
+        
+        // Imprimir los nombres solo si hay alguno
+        if ($nombresArray !== null) {
+            echo "Nombres: " . implode(", ", $nombresArray);
+        } else {
+            echo "No se proporcionaron nombres.";
+        }
+    } else {
+        
+        echo "No se han enviado datos por POST.";
+    }
+
+?>
+
+<head>
+  <title>Admin - PlacoShoes</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link href="css/finalizar_compra.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="d-flex align-items-center">
+  <div class="container">
+    <div class="row justify-content-center" style="margin:20px;">
+      <div class="col-lg-6 col-md-8 caja-login">
+        <div class="col-lg-12 caja-titulo">
+          Detalles del pago
+        </div>
+        <div class="col-lg-12 login-form">
+            <form method="post" action="procesar_pago.php">
+                
+
+                <br>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="forma_pago" id="tarjeta_credito" value="tarjeta_credito" checked>
+                    <label class="form-check-label" for="tarjeta_credito">
+                        Pago con Tarjeta de Crédito
+                    </label>
+                </div>
+
+                <!-- Campos de tarjeta de crédito (inicialmente ocultos) -->
+                <div id="campos_tarjeta" style="display: none">
+                    <div class="form-group">
+                        <label for="numero_tarjeta">Número de Tarjeta:</label>
+                        <input type="text" class="form-control" name="numero_tarjeta" id="numero_tarjeta" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nombre_titular">Nombre del Titular:</label>
+                        <input type="text" class="form-control" name="nombre_titular" id="nombre_titular" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="fecha_expiracion">Fecha de Expiración:</label>
+                        <input type="text" class="form-control" name="fecha_expiracion" id="fecha_expiracion" placeholder="MM/AA" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="codigo_seguridad">Código de Seguridad:</label>
+                        <input type="text" class="form-control" name="codigo_seguridad" id="codigo_seguridad" required>
+                    </div>
+                </div>
+
+                <!-- Opción de pago en OXXO -->
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="forma_pago" id="pago_oxxo" value="pago_oxxo">
+                        <label class="form-check-label" for="pago_oxxo">
+                            Pago en OXXO
+                        </label>
+                    <div id="campos_oxxo" style="display: none;">
+                        <br>
+                        <img src="media/logo_oxxo.png" alt="" width="150px;">
+                    </div>
+                </div>
+
+                
+                <br>
+
+                <!-- Código de cupón -->
+                <label for="cupon">Código de Cupón:</label>
+                <input type="text" name="cupon" id="cupon">
+
+                <br>
+
+                <!-- País y impuestos -->
+                <label for="pais">Selecciona tu país:</label>
+                <select name="pais" id="pais">
+                    <option value="mexico">México</option>
+                    <option value="argentina">Argentina</option>
+                </select>
+
+                <br>
+
+                <!-- Gastos de envío -->
+                <label for="envio">Gastos de Envío:</label>
+                <input type="radio" name="envio" value="gratuito" id="envio_gratuito"> <label for="envio_gratuito">Gratuito</label>
+                <input type="radio" name="envio" value="con_costo" id="envio_con_costo"> <label for="envio_con_costo">Con Costo</label>
+
+                <br>
+
+                <input class="btn2 btn2-outline-primary" type="submit" value="Pagar">
+            </form>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<script>
+    // Mostrar u ocultar campos de tarjeta de crédito según la opción seleccionada
+    $('input[name="forma_pago"]').change(function () {
+        if ($('#tarjeta_credito').is(':checked')) {
+            $('#campos_tarjeta').show();
+            $('#campos_oxxo').hide();
+
+        } else {
+            $('#campos_tarjeta').hide();
+            $('#campos_oxxo').show();
+        }
+    });
+</script>
+
+</body>
+<?php include 'footer.php'?>
