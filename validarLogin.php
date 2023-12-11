db25e815dc08712c858e73a94c3185faed382eae
DiegoRuP <96434128+DiegoRuP@users.noreply.github.com>
2023-12-11 17:20:42 -0600

    Validacion de login
    Listo

--- validarLogin.php
+++ validarLogin.php
@@ -54,72 +54,94 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $online = "Online";
 
     // Buscar en la base de datos
-    $sql = "SELECT nombre, contraseña, $online FROM usuarios WHERE cuenta = '$usuario'";
+    $sql = "SELECT nombre, contraseña, cuenta, $online FROM usuarios WHERE cuenta = '$usuario'";
+
     $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $contraseña_encriptada = $row['contraseña'];
-        $bloqueo = $row[$online];
-
-        if($bloqueo == 0){
-
-                    // Desencriptar la contraseña almacenada en la base de datos
-        $contraseña_guardada = decrypt($contraseña_encriptada);
-
-        // Verificar la contraseña
-        if ($contraseña == $contraseña_guardada) {
-            
-            // Si el checkbox "Recuérdame" está marcado, establecer la cookie
-            if ($recordar) {
-                setcookie("usuario", $usuario, time() + 3600, "/"); // La cookie expirará en 1 hora
-                setcookie("contraseña", $contraseña, time() + 3600, "/"); // La cookie expirará en 1 hora
+    
+    if ($result) {
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $nombre = $row['cuenta'];
+            $contraseña_encriptada = $row['contraseña'];
+            $bloqueo = $row[$online];
+    
+            if ($bloqueo == 0) {
+    
+                // Desencriptar la contraseña almacenada en la base de datos
+                $contraseña_guardada = decrypt($contraseña_encriptada);
+    
+                // Verificar la contraseña
+                if ($contraseña == $contraseña_guardada && $usuario == $nombre) {
+    
+                    // Si el checkbox "Recuérdame" está marcado, establecer la cookie
+                    if ($recordar) {
+                        setcookie("usuario", $usuario, time() + 3600, "/"); // La cookie expirará en 1 hora
+                        setcookie("contraseña", $contraseña, time() + 3600, "/"); // La cookie expirará en 1 hora
+                    }
+    
+                    header("Location: captcha.php");
+                    $_SESSION["usernav"] = $usuario;
+    
+                } else if ($contraseña != $contraseña_guardada) {
+                    $_SESSION["userblock"] = $usuario;
+    
+                    $intentos++;
+    
+                    // Almacenar el contador de intentos en la sesión
+                    $_SESSION['intentos'] = $intentos;
+    
+                    if ($intentos === 3) {
+    
+                        echo "<script>
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Intentos Excedido',
+                                text: 'Cuenta Bloqueada'
+                            }).then(function() {
+                                window.location.href='bloqueoCuenta.php';
+                            });
+                         </script>";
+    
+                        $_SESSION['intentos'] = 0;
+                        $_SESSION['inres'] = 3;
+    
+                    } else {
+    
+                        $inres--;
+                        $_SESSION['inres'] = $inres;
+    
+                        echo "<script>
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            text: 'Contraseña Incorrecta Intentos restantes $inres'
+                        }).then(function() {
+                            window.location.href='login.php';
+                        });
+                        </script>";
+    
+                    }
+    
+                }
+            } else {
+                // El usuario está bloqueado, puedes mostrar una alerta o realizar alguna acción específica
             }
-
-            header("Location: captcha.php");
-            $_SESSION["usernav"] = $usuario;
-            
-        } else if ($contraseña != $contraseña_guardada) {
-            $_SESSION["userblock"] = $usuario;
-
-            $intentos++;
-
-            // Almacenar el contador de intentos en la sesión
-            $_SESSION['intentos'] = $intentos;
-
-            if ($intentos === 3) {
-
-                echo "<script>
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Intentos Excedido',
-                        text: 'Cuenta Bloqueada'
-                    }).then(function() {
-                        window.location.href='bloqueoCuenta.php';
-                    });
-                 </script>";
-
-                 $_SESSION['intentos'] = 0;
-                 $_SESSION['inres'] = 3;
-                 
-            }else{
-
-                $inres--;
-                $_SESSION['inres'] = $inres;
-
-                echo "<script>
+        } else {
+            // El usuario no existe en la base de datos, mostrar alerta
+            echo "<script>
                 Swal.fire({
                     icon: 'error',
                     title: 'Error',
-                    text: 'Contraseña Incorrecta Intentos restantes $inres'
+                    text: 'El usuario no existe'
                 }).then(function() {
                     window.location.href='login.php';
                 });
                 </script>";
-
-            }
-
         }
+    } else {
+        // Manejar el caso de error en la consulta SQL
+        echo "Error en la consulta SQL: " . $conn->error;
+    }
 
 
     }else{
@@ -133,8 +155,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     });
                  </script>";
     }
-}
-}
 // Cerrar conexión
 $conn->close();
 ?>
